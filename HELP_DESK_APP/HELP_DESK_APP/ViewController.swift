142e3ef35189a580ab228ee1934532a7cb4127fd
Donovan Vince Lara Alvarado <133814320+onlydonos@users.noreply.github.com>
2024-06-03 19:52:39 -0600

    Primer commit
    Adición del proyecto de desarrollo de vistas para help desk app

---
+++ HELP_DESK_APP/HELP_DESK_APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HELP_DESK_APP
+//
+//  Created by Donovan Vince Lara Alvarado on 29/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
