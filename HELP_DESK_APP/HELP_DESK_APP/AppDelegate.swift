23df5d5b0a3706b9e35692450badbbcc86df0336
Braulio Alejandro Navarrete Horta <bnh74570@lasallebajio.edu.mx>
2024-06-17 20:28:41 -0600

    [ADD] Configuración en el app delegate.
    Configuración en App delegate para evitar que gira la aplicación por más que el telefono lo volteen.

--- HELP_DESK_APP/HELP_DESK_APP/AppDelegate.swift
+++ HELP_DESK_APP/HELP_DESK_APP/AppDelegate.swift
@@ -1,16 +1,9 @@
-//
-//  AppDelegate.swift
-//  HELP_DESK_APP
-//
-//  Created by Donovan Vince Lara Alvarado on 29/05/24.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -31,6 +24,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    // MARK: Orientation Restriction
 
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait // Cambia a .landscape si deseas solo orientación horizontal
+    }
 }
-
